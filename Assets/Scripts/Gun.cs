866b21ed78e5fb158c4e5f8dd77d6afdec9e47af
Robby Lewellen <robbylewellen25@amdg.rockhursths.edu>
2024-02-23 12:30:43 -0600

    Pickup turning and functionalities

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gun : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class Gun : MonoBehaviour
     public float gunShotRadius = 20f;
 
     public int maxAmmo;
-    private int ammo;
+    private int ammo = 10;
 
     public LayerMask raycastLayerMask;
     public LayerMask enemyLayerMask;
@@ -122,6 +123,5 @@ public class Gun : MonoBehaviour
         {
             ammo = maxAmmo;
         }
- 
     }
 }
